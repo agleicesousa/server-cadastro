789242a256c5959e387b44189910ae33db4d5c83
agleicesousa <agleicesous98@gmail.com>
2024-09-20 01:46:10 -0300

    feat: conf. iniciais da aplicação

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Welcome to this application');
+const app = require("./app");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => {
+    console.log(`App listening on http://localhost:${port}`);
+});
